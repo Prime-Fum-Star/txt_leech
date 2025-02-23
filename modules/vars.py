ed2cc595a5f1b5cb8cb40ca985683ce55f39ea72
Prime-Fum-Star <primefuntv881@gmail.com>
2025-02-23 23:39:06 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "25645046"
+api_hash = "568c366b5573c5c349f0f0f25c3a4ccd"
+bot_token = "8177990799:AAHryUHPqCZ_DPseeorGOrK6Jq9s7OeVtMo"
